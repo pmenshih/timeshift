a153adc45326655c3c1ed93c25d782334119ddd6
port <portnikhin.oleg@gmail.com>
2023-01-23 10:51:23 +0300

    A little of OOP.

--- main.py
+++ main.py
@@ -8,53 +8,8 @@
 В случае нахождения города (его корректного имени) его имя и часовой пояс
 сохраняется в локальный список программы.
 '''
-import datetime
+from provider import Abstractapi
 
-import requests
-
-
-ABSTRACTAPI_URL = (
-    'https://timezone.abstractapi.com/v1/current_time/?'
-    'api_key=30a9c393ff8c4585bec59a8a02cfe5be&location='
-)
-
-
-def find_city():
-    '''
-    Поиск города и информации о его времени с помощью abstractapi.com.
-    '''
-    city_name = input('Введите имя города: ')
-
-    city_data = requests.get(ABSTRACTAPI_URL + city_name).json()
-    if not city_data:
-        print(f'Город с именем "{city_name}" не найден.')
-        return None
-
-    print('Текущее время:', get_local_time(city_data['gmt_offset']))
-
-    return {
-        'gmt_offset': city_data['gmt_offset'],
-        'name': city_name,
-    }
-
-def get_local_time(
-    gmt_offset,
-    current_utc_time=datetime.datetime.now(datetime.timezone.utc)
-):
-    '''
-    Определение текущего времени по значению часового пояса.
-
-    Параметры:
-        - `gmt_offset`: смещение в часах относительно времени UTC;
-        - `current_utc_time`: текущее время UTC.
-    '''
-    return (
-        current_utc_time + datetime.timedelta(hours=gmt_offset)
-    ).strftime('%H:%M')
-
-def show_cities_list(cities):
-    for city_name in sorted(cities):
-        print(f'{city_name}: {get_local_time(cities[city_name])}')
 
 def show_menu():
     print(
@@ -73,13 +28,14 @@ if __name__ == '__main__':
 
         command = input('>>> ')
         print()
-
+        abstractapi = Abstractapi()
         # Вывод списка городов.
         if command == '1':
-            show_cities_list(cities)
+            abstractapi.show_cities_list(cities)
         # Поиск и добавление нового города.
         elif command == '2':
-            city_data = find_city()
+            city_name = input('Введите имя города: ')
+            city_data = abstractapi.find_city(city_name)
             if not city_data:
                 continue
             cities[city_data['name']] = city_data['gmt_offset']
---
+++ provider.py
@@ -0,0 +1,50 @@
+import requests
+import datetime
+
+
+class Provider:
+
+    def show_cities_list(self, cities):
+        for city_name in sorted(cities):
+            print(f'{city_name}: {self.get_local_time(cities[city_name])}')
+
+    def get_local_time(
+            self,
+            gmt_offset,
+            current_utc_time=datetime.datetime.now(datetime.timezone.utc)
+    ):
+        '''
+        Определение текущего времени по значению часового пояса.
+
+        Параметры:
+            - `gmt_offset`: смещение в часах относительно времени UTC;
+            - `current_utc_time`: текущее время UTC.
+        '''
+        return (
+                current_utc_time + datetime.timedelta(hours=gmt_offset)
+        ).strftime('%H:%M')
+
+
+class Abstractapi(Provider):
+    ABSTRACTAPI_URL = (
+        'https://timezone.abstractapi.com/v1/current_time/?'
+        'api_key=30a9c393ff8c4585bec59a8a02cfe5be&location='
+    )
+
+    def find_city(self, city_name):
+        '''
+        Поиск города и информации о его времени с помощью abstractapi.com.
+        '''
+
+        city_data = requests.get(self.ABSTRACTAPI_URL + city_name).json()
+        if not city_data:
+            print(f'Город с именем "{city_name}" не найден.')
+            return None
+
+        print('Текущее время:', self.get_local_time(city_data['gmt_offset']))
+
+        return {
+            'gmt_offset': city_data['gmt_offset'],
+            'name': city_name,
+        }
+
