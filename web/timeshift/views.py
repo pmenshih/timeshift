06bffa76f58401badd91c961fb81b3a6c5f8534e
Peter Menshih <p.menshih@gmail.com>
2023-01-28 12:10:08 +0100

    Добавление формы ввода имени города

--- web/timeshift/views.py
+++ web/timeshift/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        print('>>>> ', request.POST['city_name'])
     return render(request, 'timeshift/index.html')
