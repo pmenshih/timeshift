b0925ccd9f897e4a67942a419a7ab8a924215d2c
Peter Menshih <p.menshih@gmail.com>
2023-02-04 11:58:13 +0100

    Отображение информации о городе в шаблоне

--- web/timeshift/views.py
+++ web/timeshift/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 from timeshift import timeshift_lib as ts
 
 def index(request):
+    response_data = dict()
+
     if request.method == 'POST':
         city_name = request.POST['city_name']
         api = ts.AbstractAPI()
-        city_data = api.fetch_city_data(city_name)
-        print('>>>', city_data)
-    return render(request, 'timeshift/index.html')
+        response_data = api.fetch_city_data(city_name)
+
+        if not response_data.get('error', False):
+            response_data['local_time'] = api.get_local_time(
+                response_data['gmt_offset']
+            )
+        
+    return render(request, 'timeshift/index.html', response_data)
