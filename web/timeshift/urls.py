1a3c78160343f9187de421e32cc37a102855380b
Peter Menshih <p.menshih@gmail.com>
2023-02-04 11:05:55 +0100

    Создание страницы Hello world

---
+++ web/timeshift/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from timeshift import views
+
+urlpatterns = [
+    path('', views.index),
+]
--- web/web/settings.py
+++ web/web/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 
 from pathlib import Path
 
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
